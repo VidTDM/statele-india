7c6c58dc0e9f1de29f2b593ab8d95f8203b9a97f
VidTDM <realvidtdm@gmail.com>
2023-09-03 15:00:36 +0530

    fix: image loading issue

--- src/components/StatesBox.jsx
+++ src/components/StatesBox.jsx
@@ -3,7 +3,7 @@ function StatesBox() {
         <>
             <div className="statesBox">
                 <img
-                    src="../../src/assets/andhra_pradesh.png"
+                    src="./statele-india/../../src/assets/andhra_pradesh.png"
                     alt="andhra_pradesh"
                     loading="eager"
                     fetchpriority="high"
